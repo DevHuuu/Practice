1799c59fbcab4bfc3f510a5575d7417a7c675fb2
DevHuuu <DevHuuu@gmail.com>
2022-06-03 21:19:28 +0900

    Instance and Static Member

---
+++ This_is_Java/src/chap6/staticmember/Television.java
@@ -0,0 +1,12 @@
+package chap6.staticmember;
+
+//정적 초기화 블록
+public class Television {
+	static String company = "Samsung";
+	static String model = "LCD";
+	static String info;
+	
+	static {
+		info = company + "-" + model;
+	}
+}
---
+++ This_is_Java/src/chap6/staticmember/TelevisionExample.java
@@ -0,0 +1,8 @@
+package chap6.staticmember;
+
+//정적 초기화 블록
+public class TelevisionExample {
+	public static void main(String[] args) {
+		System.out.println(Television.info);
+	}
+}
