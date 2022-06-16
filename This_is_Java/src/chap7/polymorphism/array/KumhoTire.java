e1de530f64ff12f81f538eb5c6e1a41aa6bffbdb
DevHuuu <DevHuuu@gmail.com>
2022-06-16 20:14:05 +0900

    chap7 polymorphism array

---
+++ This_is_Java/src/chap7/polymorphism/array/KumhoTire.java
@@ -0,0 +1,23 @@
+package chap7.polymorphism.array;
+
+//Tire의 자식 클래스
+public class KumhoTire extends Tire{
+	//필드
+	//생성자
+	public KumhoTire(String location, int maxRotation) {
+		super(location, maxRotation);
+	}
+	//메소드
+	@Override
+	public boolean roll() {
+		++accumulaterRotation;			//메소드 실행시 마다 누적회전수 증가
+		if(accumulaterRotation<maxRotation) {
+			System.out.println(location + "KumhoTire 수명: " +
+				(maxRotation-accumulaterRotation) + "회");
+			return true;
+		} else {
+			System.out.println("***" + location + "KumhoTire 펑크 ***");
+			return false;
+		}	
+	}	
+}
---
+++ This_is_Java/src/chap7/polymorphism/field/Tire.java
@@ -0,0 +1,28 @@
+package chap7.polymorphism.field;
+
+//타이어 클래스
+public class Tire {
+	//필드
+	public int maxRotation;				//최대 회전수(타이어 수명)
+	public int accumulaterRotation;		//누적 회전수
+	public String location;				//타이어의 위치
+	
+	//생성자
+	public Tire(String location, int maxRotation) {
+		this.location = location;
+		this.maxRotation = maxRotation;
+	}
+	
+	//메소드
+	public boolean roll() {
+		++accumulaterRotation;			//메소드 실행시 마다 누적회전수 증가
+		if(accumulaterRotation<maxRotation) {
+			System.out.println(location + "Tire 수명: " +
+				(maxRotation-accumulaterRotation) + "회");
+			return true;
+		} else {
+			System.out.println("***" + location + "Tire 펑크 ***");
+			return false;
+		}	
+	}
+}
