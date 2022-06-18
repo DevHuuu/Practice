1b52f3d2a30a8bf3ec4681ab5e582889a2c59ec0
DevHuuu <DevHuuu@gmail.com>
2022-06-19 01:03:38 +0900

    chap7

---
+++ This_is_Java/src/chap7/abstract_class/Cat.java
@@ -0,0 +1,14 @@
+package chap7.abstract_class;
+
+//추상 메소드 오버라이딩
+public class Cat extends Animal{
+	public Cat() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("야옹");
+	}
+
+}
