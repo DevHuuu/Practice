47b89d658d8ce137fdf9ea8606ff58566e1ee9b8
DevHuuu <DevHuuu@gmail.com>
2022-06-26 12:55:07 +0900

    Chap8 Interface

--- This_is_Java/src/chap8/_interface/defaultmethods/MyInterface.java
+++ This_is_Java/src/chap8/_interface/defaultmethods/MyInterface.java
@@ -2,4 +2,8 @@ package chap8._interface.defaultmethods;
 
 public interface MyInterface {
 	public void method1();
+	
+	public default void method2() {
+		System.out.println("MyInterface-method2 실행");
+	}
 }
