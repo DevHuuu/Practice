718ca294d658e8d6b015e648cbbd6fc4ffbdb2e7
DevHuuu <DevHuuu@gmail.com>
2022-07-05 00:06:37 +0900

    Chap10 Exception

---
+++ This_is_Java/src/chap10/runtimeexception/NullpointerException.java
@@ -0,0 +1,8 @@
+package chap10.runtimeexception;
+
+public class NullpointerException {
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+	}
+}
