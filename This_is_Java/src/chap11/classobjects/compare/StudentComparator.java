2364c704b13a751c75ab97a5cb415d2627e65162
DevHuuu <DevHuuu@gmail.com>
2022-07-21 23:17:52 +0900

    chap11 finalize(), compare()

---
+++ This_is_Java/src/chap11/classobjects/compare/StudentComparator.java
@@ -0,0 +1,16 @@
+//package chap11.classobjects.compare;
+//
+//import java.util.Comparator;
+//
+//import chap7.student.Student;
+//
+//public class StudentComparator implements Comparator<Student> {
+//
+//	@Override
+//	public int compare(Student a, Student b) {
+//		if(a.sno<b.sno) return -1;
+//		else if(a.studentNo == b.sno) return 0;
+//		else return 1;
+//	}
+//	
+//}
